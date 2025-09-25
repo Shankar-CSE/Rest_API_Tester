e1edaa21e6c142bf832d5bcca80be565d0430bc9
Shankar-CSE <shankar.l5252@gmail.com>
2025-09-25 07:23:46 +0530

    updated run.bat for the new App-all.jar drive and tested in local database with sample data

--- Phase_2/run.bat
+++ Phase_2/run.bat
@@ -1,6 +1,6 @@
 @echo off
 cd src
-javac -cp ".;..\lib\mongodb-driver-sync-4.11.1.jar;..\lib\mongodb-driver-core-4.11.1.jar;..\lib\bson-4.11.1.jar" App.java
+javac -cp ".;..\lib\App-all.jar" App.java
 if %errorlevel% equ 0 (
-    java -cp ".;..\lib\mongodb-driver-sync-4.11.1.jar;..\lib\mongodb-driver-core-4.11.1.jar;..\lib\bson-4.11.1.jar" App
+    java -cp ".;..\lib\App-all.jar" App
 )
