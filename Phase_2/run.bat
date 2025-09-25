9429311e9ab075f357ac04f237edbe5d60a0c9a9
Shankar-CSE <shankar.l5252@gmail.com>
2025-09-25 18:21:48 +0530

    updated run.bat to store my .class files in bin folder

--- Phase_2/run.bat
+++ Phase_2/run.bat
@@ -1,6 +1,6 @@
 @echo off
 cd src
-javac -cp ".;..\lib\App-all.jar" App.java
+javac -cp ".;..\lib\App-all.jar" -d ../bin App.java
 if %errorlevel% equ 0 (
-    java -cp ".;..\lib\App-all.jar" App
+    java -cp ".;..\lib\App-all.jar"  App
 )
