61cae6189a1fdab855bd99ae39015421c527e9e1
Shankar-CSE <shankar.l5252@gmail.com>
2025-09-25 19:20:52 +0530

    undo the old drivers to libe and put created driver to drivers and updated run.bat

--- Phase_2/run.bat
+++ Phase_2/run.bat
@@ -1,6 +1,7 @@
 @echo off
 cd src
-javac -cp ".;..\lib\App-all.jar" -d ../bin App.java
+javac -cp ".;..\lib\mongodb-driver-sync-4.11.1.jar;..\lib\mongodb-driver-core-4.11.1.jar;..\lib\bson-4.11.1.jar" -d ../bin App.java
 if %errorlevel% equ 0 (
-    java -cp ".;..\lib\App-all.jar"  App
+    cd ../bin
+    java -cp ".;..\lib\mongodb-driver-sync-4.11.1.jar;..\lib\mongodb-driver-core-4.11.1.jar;..\lib\bson-4.11.1.jar" App
 )
