cb435afe676ded98325bcffa6ae7e1f2107b7423
Shankar-CSE <shankar.l5252@gmail.com>
2025-10-12 17:46:21 +0530

    copied run.bat file to run the cli app

---
+++ Phase_3/run.bat
@@ -0,0 +1,7 @@
+@echo off
+cd src
+javac -cp ".;..\lib\mongodb-driver-sync-4.11.1.jar;..\lib\mongodb-driver-core-4.11.1.jar;..\lib\bson-4.11.1.jar" -d ../bin AppCLI.java
+if %errorlevel% equ 0 (
+    cd ../bin
+    java -cp ".;..\lib\mongodb-driver-sync-4.11.1.jar;..\lib\mongodb-driver-core-4.11.1.jar;..\lib\bson-4.11.1.jar" AppCLI
+)
